7e3f9faccbd22a4086f2b2aef41da81fb7bf18c7
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-15 01:14:32 -0700

    added observability code sample for deleting code

---
+++ chapter-7/campfire-rules/src/main/java/com/github/diegopacheco/continuous/modernization/payroll/payments/OfflinePaymentProcessorToBeDeleted.java
@@ -0,0 +1,26 @@
+package com.github.diegopacheco.continuous.modernization.payroll.payments;
+
+import com.github.diegopacheco.continuous.modernization.payroll.PayrollProcessor;
+import com.github.diegopacheco.continuous.modernization.payroll.model.Employee;
+import com.github.diegopacheco.continuous.modernization.payroll.service.PayrollService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Instant;
+
+public class OfflinePaymentProcessorToBeDeleted implements PayrollProcessor {
+
+    private Logger logger = LogManager.getLogger(PayrollService.class);
+
+    @Override
+    public boolean processPayroll(Employee employee) {
+        logger.log(Level.INFO,
+                "OfflinePaymentProcessor.processPayroll" +
+                        " will be deleted! Please stop calling it!" +
+                        " Context: env: " + System.getProperty("DEPLOY_ENV").toString() +
+                        " timestamp: " + Instant.now());
+        return true;
+    }
+
+}
